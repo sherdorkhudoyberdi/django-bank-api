6b2e4ffad46714d98aa6bd0c5f96a88a88928c15
sherdor <admin@example.com>
2025-05-08 17:34:55 +0500

    chore: split settings for easy management

---
+++ config/settings/local.py
@@ -0,0 +1,31 @@
+from .base import *  # noqa
+from .base import BASE_DIR
+
+
+local_env_file = path.join(BASE_DIR, ".envs", ".env.local")
+
+if path.isfile(local_env_file):
+    load_dotenv(local_env_file)
+
+
+# SECRET_KEY = "django-insecure-qracf-02)u&7anz+=*s*gf0+@!ht!ao6uqbc6!7dpcgt6sqfbz"
+
+
+SECRET_KEY = getenv("SECRET_KEY")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = getenv("DEBUG")
+
+SITE_NAME = getenv("SITE_NAME")
+
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
+
+ADMIN_URL = getenv("ADMIN_URL")
+
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = getenv("EMAIL_HOST")
+EMAIL_PORT = getenv("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = getenv("DEFAULT_FROM_EMAIL")
+DOMAIN = getenv("DOMAIN")
+
+MAX_UPLOAD_SIZE = 1 * 1024 * 1024
